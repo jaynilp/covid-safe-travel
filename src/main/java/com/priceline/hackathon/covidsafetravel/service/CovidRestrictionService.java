ca691426754a2c09bb1fcaeca89b215c9032c885
Zeel Dedhia <81355173+zdedhia@users.noreply.github.com>
2021-08-05 19:04:03 +0530

    Delete src/main/java/com/priceline/hackathon/covidsafetravel directory
    Duplicate directory deletion

--- src/main/java/com/priceline/hackathon/covidsafetravel/service/CovidRestrictionService.java
+++
@@ -1,7 +0,0 @@
-package com.priceline.hackathon.covidsafetravel.service;
-
-import com.priceline.hackathon.covidsafetravel.domain.CovidDataResponse;
-
-public interface CovidRestrictionService {
-  CovidDataResponse getCovidRestrictions(String countryCode, String cityCode);
-}
